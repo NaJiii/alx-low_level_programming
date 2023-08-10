2c956d89cb4862276b57edc1d87b11e76f584f8b
NaJiii <tech@gmail.com>
2023-08-10 13:32:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Hello, C!\n");
+    return (0);
+}
